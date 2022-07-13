e5a5da355c202ce6f28eb26bedd6de4bf04fe792
Shimizu <emiko-s@v-pf.net>
2022-07-13 16:10:45 +0900

    commit

---
+++ app/Http/Controllers/TestController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+
+class TestControllers extends Controller
+{
+    public function add()
+    {
+        return 'add2!';
+    }    
+}
